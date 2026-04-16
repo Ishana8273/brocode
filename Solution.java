6926f94222d84c198d941763a5a55533be2c8fe3
Ishana8273 <malikjaivindra91@gmail.com>
2026-04-16 13:14:36 +0530

    contains duplicate I changed

--- Solution.java
+++ Solution.java
@@ -2,14 +2,22 @@ import java.util.*;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet <Integer> seenNumbers = new HashSet<>();
+        HashSet<Integer> seenNumbers = new HashSet<>();
 
-        for(int num : nums){
-            if(seenNumbers.contains(num)){
+        for (int num : nums) {
+            if (seenNumbers.contains(num)) {
                 return true;
             }
             seenNumbers.add(num);
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        Solution a = new Solution();
+
+        int[] nums = {1, 2, 3, 4, 2}; // example input
+
+        System.out.println(a.containsDuplicate(nums));
     }
 }
